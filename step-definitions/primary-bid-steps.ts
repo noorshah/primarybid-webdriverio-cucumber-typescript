891b78e80aaf501d79cb4b077c80b3023a42a33d
noorshah <noorsabah1988@gmail.com>
2020-07-17 11:57:45 +0100

    added assertions for news items

--- step-definitions/primary-bid-steps.ts
+++ step-definitions/primary-bid-steps.ts
@@ -3,6 +3,7 @@ import {homePage} from '../page-objects/home-page'
 import {aboutUsPage} from '../page-objects/abouts-us-page'
 import {faqsPage} from '../page-objects/faqs-page'
 import {username,password,url} from '../support/environment'
+import { newsPage } from '../page-objects/news-page';
 
 Given('I am on Primary Bid home page', function () {
     browser.url(`https://${username}:${password}@${url}`)
@@ -31,7 +32,9 @@ Then('I should be able to navigate to News page', function () {
 })
 
 Then('I should be able to see latest news about Primary Bid', function () {
-
+    expect(newsPage.newsPageHeaders).toBeDisplayed()
+    expect(newsPage.newsPageHeaders).toHaveTextContaining('Featured Content')
+    expect(newsPage.newsPageItems).toBeClickable()
 })
 
 Then('I should be able to sign up to Primary Bid', function () {
